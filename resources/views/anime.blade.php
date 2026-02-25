fd6d896e149251eda3c6734c89747b1f91a921a9
cgatlin <7417549+cgatlin@users.noreply.github.com>
2026-02-24 20:11:00 -0800

    Added foundation for API call to AniList with season and year variables

--- resources/views/anime.blade.php
+++ resources/views/anime.blade.php
@@ -1,10 +1,19 @@
 @props ([
     'season' => 'winter',
     'year' => date('Y'),
+    'animes' => [],
 ])
 
 <x-layout>
 
     <h1>You are viewing {{ $season }} - {{ $year }}</h1>
+
+    @foreach($animes as $anime)
+        <div class="anime-card">
+            <h2>{{ $anime['title']['romaji'] }} ({{ $anime['title']['native'] }})</h2>
+            <p>{{ $anime['id'] }}</p>
+        </div>
+    @endforeach
+
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+
+// Set global options for all HTTP requests
+Http::globalOptions([
+    'curl' => [
+        CURLOPT_SSL_OPTIONS => CURLSSLOPT_NATIVE_CA
+    ],
+]);
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,9 +19,53 @@ Route::get('/about', function () {
 });
 
 Route::get('/anime/{season}/{year?}', function ($season, $year = null) {
-    if ($year === null) {
-        $year = now()->year;
-    }
+    $year = $year ?? date('Y');
+    
+    // Here we define our query as a multi-line string
+    $query = 'query ($season: MediaSeason, $year: Int) {
+        Page (page: 1, perPage: 50) {
+            pageInfo {
+                total
+                perPage
+                currentPage
+                lastPage
+                hasNextPage
+            }
+            media (season: $season, seasonYear: $year, type: ANIME) {
+                id
+                season
+                seasonYear
+                title {
+                    romaji
+                    english
+                    native
+                }
+            }
+        }
+    }';
+
+    // Define our query variables and values that will be used in the query request
+    $variables = [
+        "season" => strtoupper($season),
+        "year" => $year
+    ];
+
+    $response = Http::withHeaders([
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json',
+    ])->post('https://graphql.anilist.co', [
+        'query' => $query,
+        'variables' => $variables,
+    ]);
+
+
+   
+    $data = $response->json();
+    // dd($response->successful(), $data);
+    $animes = $data['data']['Page']['media'];
+
+
     // dd($season, $year);
-    return view('anime', ['season' => $season, 'year' => $year]);
+    
+    return view('anime', ['season' => $season, 'year' => $year, 'animes' => $animes]);
 });
