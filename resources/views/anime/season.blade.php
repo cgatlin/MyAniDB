1a38d412d66955ca993819c15a8bedc8f3ad7735
cgatlin <7417549+cgatlin@users.noreply.github.com>
2026-02-26 20:57:28 -0800

    Add anime card and style the seasons page

--- resources/views/anime/season.blade.php
+++ resources/views/anime/season.blade.php
@@ -7,18 +7,22 @@
 <x-layout>
 
     <h1>You are viewing {{ $season }} - {{ $year }}</h1>
+    <ul class="mt-6 grid grid-cols-3 gap-x-10 gap-y-20 p-3" >
+        @foreach($animes as $anime)
+            
+                <x-anime-card class="anime-card" :anime="$anime" />
+            
 
-    @foreach($animes as $anime)
-        <div class="season-card">
-            <h2>{{ $anime['title']['romaji'] }}</h2>
-            <a href="/anime/{{ $anime['id'] }}">
-                <img src="{{ $anime['coverImage']['medium'] }}" alt="{{ $anime['title']['romaji'] }} Cover Image" />
-            </a>
-            <span> {{ $anime['format'] }} </span>
-            <span> {{ $anime['status'] }} </span>
-            <p>{{ $anime['description'] }}</p>
-        </div>
-    @endforeach
-
+            {{-- <div class="season-card">
+                <h2>{{ $anime['title']['romaji'] }}</h2>
+                <a href="/anime/{{ $anime['id'] }}">
+                    <img src="{{ $anime['coverImage']['medium'] }}" alt="{{ $anime['title']['romaji'] }} Cover Image" />
+                </a>
+                <span> {{ $anime['format'] }} </span>
+                <span> {{ $anime['status'] }} </span>
+                <p>{{ $anime['description'] }}</p>
+            </div> --}}
+        @endforeach
+    </ul>
 
 </x-layout>
---
+++ resources/views/components/anime-card.blade.php
@@ -0,0 +1,22 @@
+@props ([
+    'anime' => [],
+])
+
+    <div {{ $attributes->merge(['class' => 'card card-side bg-base-100 inline-grid grid-cols-2 shadow-sm']) }}>
+    <figure class="cover">
+        <a href="/anime/{{ $anime['id'] }}">
+        <img
+        src="{{ $anime['coverImage']['large'] }}"
+        alt="{{ $anime['title']['romaji'] }} Cover Image" 
+        class="w-full h-full object-cover"/>
+        </a>
+    </figure>
+    <div class="card-body data h-90">
+        <h2 class="card-title">{{ $anime['title']['romaji'] }}</h2>
+        <p class="text-sm flex-1 overflow-y-auto scrollbar-hide">{{ $anime['description'] }}</p>
+        <div class="card-actions justify-end">
+        <div class="badge badge-outline badge-info">{{ $anime['format'] }} </div>
+        <div class="badge badge-outline badge-info">{{ $anime['status'] }}</div>
+        </div>
+    </div>
+    </div>
