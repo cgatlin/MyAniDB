b1d196eb58da6dbd0d2f2580410e5f7ca164ba6c
cgatlin <7417549+cgatlin@users.noreply.github.com>
2026-03-02 20:32:23 -0800

    Sort season page by format then title and hide adult content

--- app/Http/Controllers/AniListAPIController.php
+++ app/Http/Controllers/AniListAPIController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\AniListAPIService;
+use Illuminate\Support\Collection;
+// use Illuminate\Support\Arr;
 
 class AniListAPIController extends Controller
 {
@@ -22,8 +24,26 @@ class AniListAPIController extends Controller
         $year = $year ?? date('Y');
 
         $animes = $this->animes->seasonRequest($season, $year);
-        // dd($animes);
-        return view('anime.season', ['season' => $season, 'year' => $year, 'animes' => $animes]);
+        foreach ($animes as $anime) {
+            if ($anime['isAdult']) {
+                $typeSortAnime['isAdult'][]= $anime;
+            } else {
+                $typeSortAnime[$anime['format']][]= $anime;
+            }
+        }
+        // dd($typeSortAnime);
+        // $sortedArray = Arr::sort($typeSortAnime, function ($item) {
+        //     return $item['title']['romaji'];
+        // });
+        // $sortedArray = collect($typeSortAnime)->sortBy('title.romaji', SORT_NATURAL)->all();
+        $sortedData = collect($typeSortAnime)->map(function ($items) {
+            return collect($items)
+                ->sortBy('title.romaji', SORT_NATURAL | SORT_FLAG_CASE)
+                ->values() // Optional: resets 0, 1, 2... keys within each category
+                ->all();
+        })->all();
+        // dd($sortedData);
+        return view('anime.season', ['season' => $season, 'year' => $year, 'animes' => $sortedData]);
     }
 
 
--- resources/views/anime/season.blade.php
+++ resources/views/anime/season.blade.php
@@ -6,23 +6,57 @@
 
 <x-layout>
 
-    <h1>You are viewing {{ $season }} - {{ $year }}</h1>
-    <ul class="mt-6 grid grid-cols-3 gap-x-10 gap-y-20 p-3" >
-        @foreach($animes as $anime)
-            
-                <x-anime-card class="anime-card" :anime="$anime" />
-            
+    <h1 class="pb-2">You are viewing {{ $season }} - {{ $year }}</h1>
 
-            {{-- <div class="season-card">
-                <h2>{{ $anime['title']['romaji'] }}</h2>
-                <a href="/anime/{{ $anime['id'] }}">
-                    <img src="{{ $anime['coverImage']['medium'] }}" alt="{{ $anime['title']['romaji'] }} Cover Image" />
-                </a>
-                <span> {{ $anime['format'] }} </span>
-                <span> {{ $anime['status'] }} </span>
-                <p>{{ $anime['description'] }}</p>
-            </div> --}}
+    <p class="text-4xl font-bold">TV</p>
+    <ul class="mt-6 grid grid-cols-3 gap-x-10 gap-y-20 p-3" >
+        @foreach($animes['TV'] as $anime)
+            <x-anime-card class="anime-card" :anime="$anime" />
         @endforeach
     </ul>
+    <p class="text-4xl font-bold">TV_SHORT</p>
+    <ul class="mt-6 grid grid-cols-3 gap-x-10 gap-y-20 p-3" >
+        @foreach($animes['TV_SHORT'] as $anime)
+            <x-anime-card class="anime-card" :anime="$anime" />
+        @endforeach
+    </ul>
+    <p class="text-4xl font-bold">ONA</p>
+    <ul class="mt-6 grid grid-cols-3 gap-x-10 gap-y-20 p-3" >
+        @foreach($animes['ONA'] as $anime)
+            <x-anime-card class="anime-card" :anime="$anime" />
+        @endforeach
+    </ul>
+    <p class="text-4xl font-bold">OVA</p>
+    <ul class="mt-6 grid grid-cols-3 gap-x-10 gap-y-20 p-3" >
+        @foreach($animes['OVA'] as $anime)
+            <x-anime-card class="anime-card" :anime="$anime" />
+        @endforeach
+    </ul>
+    <p class="text-4xl font-bold">SPECIAL</p>
+    <ul class="mt-6 grid grid-cols-3 gap-x-10 gap-y-20 p-3" >
+        @foreach($animes['SPECIAL'] as $anime)
+            <x-anime-card class="anime-card" :anime="$anime" />
+        @endforeach
+    </ul>
+    <p class="text-4xl font-bold">MOVIE</p>
+    <ul class="mt-6 grid grid-cols-3 gap-x-10 gap-y-20 p-3" >
+        @foreach($animes['MOVIE'] as $anime)
+            <x-anime-card class="anime-card" :anime="$anime" />
+        @endforeach
+    </ul>
+
+    <p class="text-4xl font-bold hidden">ADULT</p>
+    <ul class="mt-6 grid grid-cols-3 gap-x-10 gap-y-20 p-3 hidden" >
+        @foreach($animes['isAdult'] as $anime)
+            <x-anime-card class="anime-card" :anime="$anime" />
+        @endforeach
+    </ul>
+
+
+    {{-- <ul class="mt-6 grid grid-cols-3 gap-x-10 gap-y-20 p-3" >
+        @foreach($animes as $anime)
+            <x-anime-card class="anime-card" :anime="$anime" />
+        @endforeach
+    </ul> --}}
 
 </x-layout>
